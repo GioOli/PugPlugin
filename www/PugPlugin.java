c2022c28732b341e10d2604c05203f10950d1a87
GioOli <72828285+GioOli@users.noreply.github.com>
2022-12-28 21:43:06 +0000

    Create PugPlugin.java

---
+++ www/PugPlugin.java
@@ -0,0 +1,22 @@
+package com.stan.cordova.plugin;
+// The native Toast API
+import android.widget.Toast;
+// Cordova-required packages
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class PugPlugin extends CordovaPlugin{
+    @Override
+  public String execute(String action, String message,
+    final CallbackContext callbackContext) {
+      String endstr = message + ": Yo malabarismatic";
+      // Send a positive result to the callbackContext
+      PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
+      callbackContext.sendPluginResult(endstr);
+      return true;
+  }
+}
